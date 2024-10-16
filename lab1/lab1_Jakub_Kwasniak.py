390f1b92f6b60247254f9fd07bfc019abf1f5c54
Jakub Kwaśniak <kubak04@outlook.com>
2024-10-16 19:32:55 +0200

    added visualizer for plotting 2D and 3D functions

--- lab1/lab1_Jakub_Kwasniak.py
+++ lab1/lab1_Jakub_Kwasniak.py
@@ -1,55 +1,94 @@
 # 331396
 # A - 6, B - 9, C - 3
 
-
-import math
 import random
-# import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
 
+class GradientDescent:
+    def __init__(self, points, learning_rate, num_iterations, function_grad):
+        self.points = points
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.function_grad = function_grad
 
-def function_f(points):
-    x = points
-    return 6*x + 9*math.sin(x)
+    def grad_descent(self):
+        trajectory = [self.points.copy()]
+        for _ in range(self.num_iterations):
+            self.points = self.points - self.learning_rate * self.function_grad(self.points)
+            trajectory.append(self.points.copy())
+        return trajectory
 
+class Function_f:
+    def __init__(self):
+        pass
+    def function_f(self, points):
+        x = points
+        return 6*x + 9*np.sin(x)
 
-def gradient_f(points):
-    x = points
-    return [6 - 9*math.cos(x)]
+    def gradient_f(self, points):
+        x = points
+        return [6 - 9*np.cos(x)]
 
+class Function_g:
+    def __init__(self):
+        pass
 
-def function_g(points):
-    x, y = points
-    return 3*x*y / (math.exp(x**2 + y**2))
+    def function_g(self, points):
+        x, y = points
+        return 3*x*y / (np.exp(x**2 + y**2))
 
-
-def gradient_g(points):
-    x, y = points
-    return [(3*y - 6*x**2*y) / (math.exp(x**2 + y**2)),
-            (3*x - 6*x*y**2) / (math.exp(x**2 + y**2))]
-
-
-def grad_descent(points, learning_rate, num_iterations, function_grad):
-    trajectory = [points.copy()]
-    for _ in range(num_iterations):
-        points = points - learning_rate * function_grad(points)
-        trajectory.append(points.copy())
-    return trajectory
+    def gradient_g(self, points):
+        x, y = points
+        return [(3*y - 6*x**2*y) / (np.exp(x**2 + y**2)),
+                (3*x - 6*x*y**2) / (np.exp(x**2 + y**2))]
+    
+class DataVisualizer:
+    def __init__(self):
+        pass
+    
+    def Visualize_2D(self, X, Y):
+        plt.figure(figsize=(6, 10))
+        plt.plot(X, Y)
+        plt.title('F(x) plot')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+    
+    def Visualize_3D(self, X, Y, Z):
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(projection='3d')
+        ax.plot_surface(X, Y, Z, cmap='inferno')
+        ax.set_title('G(x) plot')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('G(X, Y)')
+        plt.show()
 
 
 if __name__ == "__main__":
 
-    f_section = [-4*math.pi, 4*math.pi]
-    g_section = [(-2, 2), (-2, 2)]
-
-    points_f = [random.uniform(f_section[0], f_section[1])]
+    eps = 1e-10
+    points_f = [random.uniform(-4*np.pi + eps, 4*np.pi - eps)]
     points_g = [
-        random.uniform(g_section[0][0], g_section[0][1]),
-        random.uniform(g_section[1][0], g_section[1][1])
+        random.uniform(-2 + eps, 2 - eps), random.uniform(-2 + eps, 2 - eps)
         ]   
     
-    func_f = function_f
-    func_g = function_g
+    func_f = Function_f()
+    func_g = Function_g()
 
-    grad_descent(points_f, 0.1, 1000, func_f)
-    grad_descent(points_g, 0.1, 1000, func_g)
+    trajectory_f = GradientDescent(points_f, 0.1, 1000, func_f.gradient_f)
+    trajectory_g = GradientDescent(points_g, 0.1, 1000, func_g.gradient_g)
    
+    plotter = DataVisualizer()
+    
+    # g_x = np.linspace(-2, 2)
+    # g_y = np.linspace(-2, 2)
+    # X, Y = np.meshgrid(g_x, g_y)
+    # Z = func_g.function_g([X, Y])
+    # plotter.Visualize_3D(X, Y, Z)
+
+    X = np.linspace(-4*np.pi, 4*np.pi)
+    Y = func_f.function_f(X)
+    plotter.Visualize_2D(X, Y)
+
