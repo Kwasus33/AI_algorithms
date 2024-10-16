9c6b1c0c38a3b604224b1d5df08142e6e9113a4a
Jakub Kwaśniak <kubak04@outlook.com>
2024-10-16 18:28:44 +0200

    added universal grad_descent and main

--- lab1/lab1_Jakub_Kwasniak.py
+++ lab1/lab1_Jakub_Kwasniak.py
@@ -3,19 +3,53 @@
 
 
 import math
+import random
+# import numpy as np
 
 
-def function_f(x):
+def function_f(points):
+    x = points
     return 6*x + 9*math.sin(x)
 
 
-def gradient_f(x):
-    return 6 - 9*math.cos(x)
+def gradient_f(points):
+    x = points
+    return [6 - 9*math.cos(x)]
 
 
-def function_g(x, y):
+def function_g(points):
+    x, y = points
     return 3*x*y / (math.exp(x**2 + y**2))
 
 
-def gradient_g(x, y):
-    return 
+def gradient_g(points):
+    x, y = points
+    return [(3*y - 6*x**2*y) / (math.exp(x**2 + y**2)),
+            (3*x - 6*x*y**2) / (math.exp(x**2 + y**2))]
+
+
+def grad_descent(points, learning_rate, num_iterations, function_grad):
+    trajectory = [points.copy()]
+    for _ in range(num_iterations):
+        points = points - learning_rate * function_grad(points)
+        trajectory.append(points.copy())
+    return trajectory
+
+
+if __name__ == "__main__":
+
+    f_section = [-4*math.pi, 4*math.pi]
+    g_section = [(-2, 2), (-2, 2)]
+
+    points_f = [random.uniform(f_section[0], f_section[1])]
+    points_g = [
+        random.uniform(g_section[0][0], g_section[0][1]),
+        random.uniform(g_section[1][0], g_section[1][1])
+        ]   
+    
+    func_f = function_f
+    func_g = function_g
+
+    grad_descent(points_f, 0.1, 1000, func_f)
+    grad_descent(points_g, 0.1, 1000, func_g)
+   
