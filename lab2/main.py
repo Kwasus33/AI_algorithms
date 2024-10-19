6258dbd4e8acfff0ddad69d7e623587772b25377
Bartlomiej Olber <bartlomiej.olber.stud@pw.edu.pl>
2024-10-19 20:43:17 +0200

    refactor main

--- lab2/main.py
+++ lab2/main.py
@@ -23,11 +23,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    if args.seed is not None:
-        np.random.seed(args.seed)
-
+def load_data(args):
     data = pd.read_csv(args.cities_path)
     data_without_start_and_finish = data[~((data.index == args.finish) | (data.index == args.start))]
     if args.problem_size == "mini":
@@ -37,7 +33,15 @@ def main():
     else:
         city_names = [args.start] + data_without_start_and_finish.index.tolist() + [args.finish]
 
-    data = data[city_names].loc[city_names]
+    return data[city_names].loc[city_names]
+
+
+def main():
+    args = parse_args()
+    if args.seed is not None:
+        np.random.seed(args.seed)
+
+    data = load_data(args)
     print(generate_solution(data))
 
 
