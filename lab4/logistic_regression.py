58603646228dbd7ebb90a0bab9c31aa3df3d7bb0
Jakub Kwaśniak <kubak04@outlook.com>
2024-12-06 15:12:53 +0100

    adds data prep and logistic regression implementation

--- lab4/logistic_regression.py
+++ lab4/logistic_regression.py
@@ -1,12 +1,35 @@
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
-from sklearn.metrics.pairwise import sigmoid_kernel
+import numpy as np
 
 
 class LogisticRegression:
-    def __init__(self, data):
-        self.data = data
-        self.X = self.data.data.features
-        self.Y = self.data.data.targets
-        
-    def sigmoid(self, X, Y):
-        return sigmoid_kernel(X, Y)
+    def __init__(self, learning_rate, n_iters, threshold):
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.threshold = threshold
+
+    def _sigmoid(self, x):
+        x = np.clip(x)
+        return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y):
+        x_samples, x_features = X.shape  # (rows, columns)
+        self.weights = np.zeros(x_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = self._sigmoid(
+                linear_pred
+            )  # linear combination of all features is sigmoid arg
+
+            # derivates of cross entropy loss which we minimize
+            dw = (1 / x_samples) * np.dot(X.T, (predictions - y))
+            db = (1 / x_samples) * np.sum(predictions - y)
+
+            self.weights = self.weights - self.learning_rate * dw
+            self.bias = self.bias - self.learning_rate * db
+
+    def predict(self, X):
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_pred = self._sigmoid(linear_pred)
+        return np.array([1 if pred > self.threshold else 0 for pred in y_pred])
--- lab4/main.py
+++ lab4/main.py
@@ -1,15 +1,67 @@
 from ucimlrepo import fetch_ucirepo
 from logistic_regression import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+import numpy as np
+import argparse
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--learning-rate", "-lr", type=float, required=True)
+    parser.add_argument("--iters", "-i", type=int, required=True)
+    parser.add_argument("--threshold", "-th", type=float, required=True)
+    parser.add_argument("--seed", type=int)
+    return parser.parse_args()
 
 
 def load_data():
     cancer_data = fetch_ucirepo(id=17)
-    return cancer_data
+    data_df = cancer_data.data.features
+    targets_df = cancer_data.data.targets
+
+    # any_missing_data = data_df.isnull().any()
+    # any_missing_targets = target_df.isnull().any()
+    # print(f"{any_missing_data}\n")
+    # print(f"{any_missing_targets}\n")
+
+    return (data_df, targets_df)
+
+
+def prep_data(seed):
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.75, random_state=seed
+    )
+    y_train = np.array([1 if y == "M" else 0 for y in y_train["Diagnosis"]])
+    y_test = np.array([1 if y == "M" else 0 for y in y_test["Diagnosis"]])
+    return (X_train, X_test, y_train, y_test)
 
 
 def main():
-    data = load_data()
-    model = LogisticRegression(data)
+    args = parseArgs()
+
+    if args.seed is not None:
+        seed = args.seed
+    else:
+        seed = 1
+
+    X_train, X_test, y_train, y_test = prep_data(seed)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+
+    model = LogisticRegression(args.learning_rate, args.iters, args.threshold)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy:.3f}")
+    print(f"F-score: {f1:.3f}")
 
 
 if __name__ == "__main__":
