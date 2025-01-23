9df7a6829d9ca4c348d0824ddd9f6fd74f14e292
Jakub Kwaśniak <kubak04@outlook.com>
2025-01-23 22:29:02 +0100

    created model with pomegranate lib

--- lab7/main.py
+++ lab7/main.py
@@ -3,13 +3,37 @@ import pandas as pd
 import os
 import pomegranate
 
+# import bnlearn
 
-def load_data():
+
+def load_data(file_path, columns):
+
+    crimes_df = pd.read_csv(
+        file_path, usecols=columns, low_memory=False
+    )  # low_memory=false cause Dtypes are mixed
+
+    # crimes_df = crimes_df[
+    #     ~crimes_df.isin(["Unknown"]).any(
+    #         axis=1
+    #     )  # takes only lines where Uknown doesn't appear in any cell
+    # ]
+    # crimes_df.dropna(inplace=True)
+
+    crimes_df = crimes_df.replace("Unknown", pd.NA).dropna()
+
+    return crimes_df
+
+
+def net_learn(crimes_df, states):
+    net = pomegranate.BayesianNetwork.from_samples(
+        crimes_df, state_names=states, algorithm="exact"
+    )
+    return net
+
+
+def main():
     dir_path = kagglehub.dataset_download("mrayushagrawal/us-crime-dataset")
     file_path = os.path.join(dir_path, "US_Crime_DataSet.csv")
-    crimes_df = pd.read_csv(
-        file_path, low_memory=False
-    )  # low_memory=false cause Dtypes are mixed
     columns = [
         "Victim Sex",
         "Victim Age",
@@ -20,18 +44,12 @@ def load_data():
         "Relationship",
         "Weapon",
     ]
-    crimes_df = crimes_df.filter(columns, axis=1).dropna(inplace=True)
-    # print(crimes_df)
-
-    return crimes_df
-
-
-def net():
-    net = pomegranate.BayesianNetwork()
-
-
-def main():
-    crimes_df = load_data()
+    crimes_df = load_data(file_path, columns)
+    model = net_learn(crimes_df, columns)
+    print(model.structure)
+    for node in model.states:
+        print(node.name)
+        print(f"{node.distribution}\n")
 
 
 if __name__ == "__main__":
